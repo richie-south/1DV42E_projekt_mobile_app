a21040b2d9799f027351fe0e432ed0b65c740317
rs222kn <richard.soderman@gmail.com>
2016-04-17 19:13:27 +0200

    fixed small code errors and changed swipe library to scrollabletabview

--- IvyTown/pages/MyCards.android.js
+++ IvyTown/pages/MyCards.android.js
@@ -12,41 +12,65 @@ import React, {
     TouchableNativeFeedback
 } from 'react-native';
 
-import SwipeableViews from 'react-swipeable-views/lib/index.native.animated';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+
 import { Actions } from 'react-native-router-flux';
 import styles from '../styles/MyCardsStyle';
 import Card from '../components/Card';
+import config from '../config';
 
-/**
- * [handles card presses, had to put this outside of calss]
- * @param  {[object]} card [pressed card object]
- */
-const onCardPress = (card) => {
-    console.log(card);
+// socket config fix
+if (window.navigator && Object.keys(window.navigator).length == 0) {
+  window = Object.assign(window, { navigator: {userAgent: 'ReactNative'}});
+}
+
+import io from 'socket.io-client/socket.io';
+const socket = io(config.url, {
+    transports: ['websocket'], forceNew: true
+});
+/*
+const io = {};
+const socket = {};
+*/
+const onPress = (e) => {
+    console.log(e);
+};
+
+const onCardLobbyPress = (e) => {
+    console.log('cardLobby', e);
 };
 
 export default class MyCards extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            dataSource: new ListView.DataSource({
+            myCards: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            lobbyCards: new ListView.DataSource({
                 rowHasChanged: (row1, row2) => row1 !== row2,
             }),
             loaded: false
         };
     }
 
-
     componentDidMount() {
-        this.fetchData(this.props.data.fbId);
+        this.fetchData(this.props.data.fbId)
+            .then(() => {
+                this.socketLobby();
+            })
+            .catch((e) => {
+                console.log('Fetch error', e);
+            });
+
     }
 
     /**
      * [retrives card data]
      */
     fetchData(fbId) {
-        const url = 'http://194.47.105.68:3334/user/cards/'+fbId;
-        new Promise((resolve, reject) => {
+        const url = config.url+'/user/cards/'+fbId;
+        return new Promise((resolve, reject) => {
             const request = new XMLHttpRequest();
             request.onreadystatechange = (e) => {
                 if (request.readyState !== 4) {
@@ -57,9 +81,10 @@ export default class MyCards extends Component{
                     const cards = JSON.parse(request.responseText);
                     console.log(cards);
                     this.setState({
-                        dataSource: this.state.dataSource.cloneWithRows(JSON.parse(request.responseText)),
+                        myCards: this.state.myCards.cloneWithRows(JSON.parse(request.responseText)),
                         loaded: true
                     });
+                    resolve(true);
                 } else {
                     reject(e);
                 }
@@ -67,7 +92,49 @@ export default class MyCards extends Component{
 
             request.open('GET', url);
             request.send();
-        }).done();
+        });
+    }
+
+    socketJoinLobby(){
+        socket.emit('room','lobby');
+    }
+
+    socketLobbyUpdate(){
+        socket.on('update', (cards) => {
+            console.log('cards', cards);
+            this.setState({
+                lobbyCards: this.state.lobbyCards.cloneWithRows(cards)
+            });
+        });
+    }
+
+    addCardToLobby(card){
+        const mockCard = {
+            _id: '5703b3bbd61b52010ec3f591',
+            _creator: '5703b3bbd61b52010ec3f590',
+            name: 'Richard',
+            avatar: 'richards bild',
+            pastUsers: [],
+            stats: {
+                blockBoost: 0,
+                healBoost: 0,
+                attackBoost: 10,
+                heal: 20,
+                block: 30,
+                attack: 40,
+                xp: 0,
+                lvl: 0
+            },
+            backgroundCardImg: 'defaultCardImg'
+        };
+
+        socket.emit('lobby', { add: true, card});
+    }
+
+    socketLobby(){
+        console.log('socketLobby');
+        this.socketJoinLobby();
+        this.socketLobbyUpdate();
     }
 
     render() {
@@ -76,19 +143,22 @@ export default class MyCards extends Component{
         }
 
         return (
-            <SwipeableViews>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={this.renderCard}
-                    style={styles.listView}
-                />
+            <ScrollableTabView renderTabBar={false}>
 
                 <ListView
-                    dataSource={this.state.dataSource}
+                    tabLabel="deck"
+                    dataSource={this.state.myCards}
                     renderRow={this.renderCard}
-                    style={styles.listView}
+                    contentContainerStyle={styles.listView}
                 />
-            </SwipeableViews>
+                <ListView
+                    tabLabel="lobby"
+                    dataSource={this.state.lobbyCards}
+                    renderRow={this.renderLobbyCards}
+                    contentContainerStyle={styles.listView}
+                />
+
+            </ScrollableTabView>
         );
     }
 
@@ -102,9 +172,15 @@ export default class MyCards extends Component{
       );
     }
 
+    renderLobbyCards(card){
+        return (
+            <Card onPress={onCardLobbyPress} card={card}/>
+        );
+    }
+
     renderCard(card) {
       return (
-          <Card card={card}/>
+          <Card onPress={onPress} card={card}/>
       );
     }
 }
