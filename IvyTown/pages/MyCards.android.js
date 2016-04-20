1456df4012728a2c9beb6db422760e20625aed64
rs222kn <richard.soderman@gmail.com>
2016-04-20 14:54:50 +0200

    add and remove to lobby functionality added

--- IvyTown/pages/MyCards.android.js
+++ IvyTown/pages/MyCards.android.js
@@ -17,13 +17,13 @@ import ScrollableTabView from 'react-native-scrollable-tab-view';
 import { Actions } from 'react-native-router-flux';
 import styles from '../styles/MyCardsStyle';
 
-//import Card from '../components/Card';
 import LobbyCard from '../components/LobbyCard';
 import CardView from '../components/CardView';
 import GridView from 'react-native-grid-view';
-
-
+import Rx from 'rx';
 import config from '../config';
+
+const lobby = 'lobby';
 
 // socket config fix
 if (window.navigator && Object.keys(window.navigator).length == 0) {
@@ -35,13 +35,6 @@ const socket = io(config.url, {
     transports: ['websocket'], forceNew: true
 });
 
-const onPress = (e) => {
-    console.log(e);
-};
-
-const onCardLobbyPress = (e) => {
-    console.log('cardLobby', e);
-};
 
 export default class MyCards extends Component{
     constructor(props) {
@@ -55,15 +48,8 @@ export default class MyCards extends Component{
         };
     }
 
-    componentDidMount() {
-        this.fetchData(this.props.data.fbId)
-            .then(() => {
-                this.socketLobby();
-            })
-            .catch((e) => {
-                console.log('Fetch error', e);
-            });
-
+    componentWillMount() {
+        this.fetchData(this.props.data.fbId);
     }
 
     /**
@@ -71,76 +57,75 @@ export default class MyCards extends Component{
      */
     fetchData(fbId) {
         const url = config.url+'/user/cards/'+fbId;
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.onreadystatechange = (e) => {
-                if (request.readyState !== 4) {
-                    return;
-                }
-
-                if (request.status === 200) {
-                    const cards = JSON.parse(request.responseText);
-                    console.log(cards);
-                    this.setState({
-                        myCards: this.state.myCards.cloneWithRows(JSON.parse(request.responseText)),
-                        loaded: true
-                    });
-                    resolve(true);
-                } else {
-                    reject(e);
-                }
-            };
-
-            request.open('GET', url);
-            request.send();
-        });
+        fetch(url)
+            .then(response => {
+                setTimeout(() => null, 0); // react native bugg workaround
+                return response.json();})
+            .then((myCards) => {
+                console.log('my cards', myCards);
+                this.setState({
+                    myCards: this.state.myCards.cloneWithRows(myCards),
+                    loaded: true
+                });
+                this.socketLobby();
+            })
+            .catch((e) => console.log(e));
     }
 
     socketJoinLobby(){
-        socket.emit('room','lobby');
+        socket.emit('room', lobby);
     }
 
     socketLobbyUpdate(){
         socket.on('update', (cards) => {
-            console.log('cards', cards);
-            let c = cards.map((card, index) => {
-                card.key = index+'c';
-                return card;
-            });
+            console.log('lobby cards', cards);
             this.setState({
-                lobbyCards: c
+                lobbyCards: cards.map((card, index) => {
+                    card.key = card._id;
+                    return card;
+                }).slice(0)
             });
         });
     }
 
-    addCardToLobby(card){
-        const mockCard = {
-            _id: '5703b3bbd61b52010ec3f591',
-            _creator: '5703b3bbd61b52010ec3f590',
-            name: 'Richard',
-            avatar: 'richards bild',
-            pastUsers: [],
-            stats: {
-                blockBoost: 0,
-                healBoost: 0,
-                attackBoost: 10,
-                heal: 20,
-                block: 30,
-                attack: 40,
-                xp: 0,
-                lvl: 0
-            },
-            backgroundCardImg: 'defaultCardImg'
-        };
-
-        socket.emit('lobby', { add: true, card});
-    }
 
     socketLobby(){
-        console.log('socketLobby');
         this.socketJoinLobby();
         this.socketLobbyUpdate();
     }
+
+    /**
+     * [add card to lobby]
+     * @param  {[object]} card [object of card pressed]
+     */
+    onAddLobbyCard(card) {
+        socket.emit(lobby, { add: true, card });
+    }
+
+    /**
+     * [remove card from lobby]
+     * @param  {[object]} card [object of card pressed]
+     */
+    onRemoveLobbyCard(card) {
+        socket.emit(lobby, { add: false, card });
+    }
+
+    /**
+     * [Pressed on card past owner btn]
+     * @param  {[object]} card [object of card pressed]
+     */
+    onPassCardOwner(card) {
+        console.log('past owners', card);
+    }
+
+    /**
+     * [press on card in lobby]
+     * @param  {[object]} card [object of card pressed]
+     */
+    onCardLobbyPress(card) {
+        console.log('cardLobby', card);
+    }
+
 
     render() {
         if (!this.state.loaded) {
@@ -148,38 +133,31 @@ export default class MyCards extends Component{
         }
 
         return (
-            <ScrollableTabView initialPage={2} renderTabBar={false}>
+            <ScrollableTabView renderTabBar={false}>
 
                 <ListView
                     tabLabel="deck"
                     dataSource={this.state.myCards}
-                    renderRow={this.renderCard}
+                    renderRow={this.renderMyCard.bind(this)}
                     contentContainerStyle={styles.listView}
                 />
 
                 <GridView
                     items={this.state.lobbyCards}
                     itemsPerRow={2}
-                    renderItem={this.renderLobbyCards}
+                    renderItem={this.renderLobbyCards.bind(this)}
                     style={styles.lobbyListView}
                   />
 
             </ScrollableTabView>
         );
-
-        /*<ListView
-              tabLabel="lobby"
-              dataSource={this.state.lobbyCards}
-              renderRow={this.renderLobbyCards}
-              contentContainerStyle={styles.lobbyListView}
-          />*/
     }
 
     renderLoadingView() {
       return (
         <View style={styles.container}>
           <Text>
-            Loading cards...
+              Loading your cards...
           </Text>
         </View>
       );
@@ -187,15 +165,16 @@ export default class MyCards extends Component{
 
     renderLobbyCards(card){
         return (
-            <LobbyCard key={card.key} onPress={onCardLobbyPress} card={card}/>
+            <LobbyCard key={card.key} onPress={this.onCardLobbyPress.bind(this)} card={card}/>
         );
     }
 
-    renderCard(card) {
+    renderMyCard(card) {
       return (
           <CardView
-              onPressAddToLobby={onPress}
-              onPressViewOwners={onPress}
+              onPressAddToLobby={this.onAddLobbyCard.bind(this)}
+              onPressRemoveFromLobby={this.onRemoveLobbyCard.bind(this)}
+              onPressViewOwners={this.onPassCardOwner.bind(this)}
               card={card}/>
       );
     }
