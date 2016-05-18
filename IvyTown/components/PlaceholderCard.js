160a3cb2458eddaf2d92c75a50032bd0be22b32c
rs222kn <rs222kn@student.lnu.se>
2016-05-18 14:35:35 +0200

    implemented placeholder card

--- IvyTown/components/PlaceholderCard.js
+++ IvyTown/components/PlaceholderCard.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import {AppRegistry, StyleSheet, Text, Image, View, ListView} from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import styles from '../styles/ChallangeViewStyle';
 import config from '../config';
 
 export default class PlaceholderCard extends Component{
@@ -10,18 +11,25 @@ export default class PlaceholderCard extends Component{
 
     }
 
-    componentWillMount() {
-    }
-
-    getTypeOfCard(){
-
-    }
-
     render() {
         return (
-            <View>
-                <Text>Placeholder card</Text>
-                {this.props.children}
+            <View style={styles.placeholderCard}>
+                <View style={styles.placeholderImageWrap}>
+                    <Image
+                        source={require('../images/sword_icon.png')}
+                        style={[styles.placeholderImage, {tintColor: 'rgba(99, 99, 99, 0.54)' }]} />
+
+                    <Image
+                        source={require('../images/bottle_icon.png')}
+                        style={[styles.placeholderImage, {tintColor: 'rgba(99, 99, 99, 0.54)' }]} />
+
+                    <Image
+                        source={require('../images/shield_icon.png')}
+                        style={[styles.placeholderImage, {tintColor: 'rgba(99, 99, 99, 0.54)' }]} />
+
+                </View>
+
+                <View style={styles.cardInPlaceholder}>{this.props.children}</View>
             </View>
         );
     }
