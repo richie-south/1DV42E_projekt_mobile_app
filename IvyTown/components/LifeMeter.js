59b4360b4df01ac07a7468f1a29f0bd4b6eb4cd4
rs222kn <rs222kn@student.lnu.se>
2016-05-18 17:01:47 +0200

    LifeMeter structure implemented

---
+++ IvyTown/components/LifeMeter.js
@@ -0,0 +1,18 @@
+'use strict';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, Image, View, ListView} from 'react-native';
+import styles from '../styles/ChallangeViewStyle';
+
+export default class LifeMeter extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+    }
+
+
+    render() {
+    }
+}
