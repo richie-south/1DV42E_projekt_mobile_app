b4fa11eb43541eee42159d407b1c4cc5af78f00b
rs222kn <rs222kn@student.lnu.se>
2016-05-19 16:32:48 +0200

    completeButton Implemented

---
+++ IvyTown/components/CompleteButton.js
@@ -0,0 +1,50 @@
+'use strict';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, Image, View, ListView, TouchableNativeFeedback} from 'react-native';
+import styles from '../styles/ChallangeViewStyle';
+import * as Animatable from 'react-native-animatable';
+
+export default class CompleteButton extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            btnText: 'Done',
+            render: true
+        };
+    }
+
+    componentWillMount() {
+    }
+
+    removeBtn(){
+        this.setState({
+            render: false
+        });
+    }
+
+    doRender(array){
+        const isTrue = array.filter(a => a);
+        return isTrue.length === array.length;
+    }
+
+    render() {
+        if(!this.doRender(this.props.doRender) || !this.state.render){
+            return false;
+        }
+
+        return (
+            <TouchableNativeFeedback
+                onPress={() => {
+                    this.removeBtn();
+                    this.props.onClick();
+                }}>
+
+                <View style={styles.doneButton}>
+                    <Text style={styles.doneButtonText}>{this.state.btnText}</Text>
+                </View>
+            </TouchableNativeFeedback>
+
+        );
+    }
+}
