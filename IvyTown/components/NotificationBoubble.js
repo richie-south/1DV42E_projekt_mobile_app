f152c93113cec93a51b2bdb484679865b5f35b9b
rs222kn <rs222kn@student.lnu.se>
2016-05-19 12:39:06 +0200

    added props to notificationBoubble, a little hardcoded values, might get fixed in future

--- IvyTown/components/NotificationBoubble.js
+++ IvyTown/components/NotificationBoubble.js
@@ -30,29 +30,74 @@ export default class NotificationBoubble extends Component{
             { width: 46 };
     }
 
-    getBorderRadius(bottom){
-        return bottom ? {
-            bottom: -34,
-            right:30,
-            borderBottomLeftRadius: 44,
-            borderBottomRightRadius: 44,
-            borderTopLeftRadius: 44,
-            borderTopRightRadius: 0,
-        }: {
-            top: -34,
-            right:30,
-            borderBottomLeftRadius: 44,
-            borderBottomRightRadius: 0,
-            borderTopLeftRadius: 44,
-            borderTopRightRadius: 44,
-        };
+    getBorderRadius(bottom, reverse){
+        if(bottom && reverse){
+            return {
+                bottom: -34,
+                left:30,
+                borderBottomLeftRadius: 44,
+                borderBottomRightRadius: 44,
+                borderTopLeftRadius: 0,
+                borderTopRightRadius: 44,
+            };
+       } else if(bottom){
+           return {
+              bottom: -34,
+              right:30,
+              borderBottomLeftRadius: 44,
+              borderBottomRightRadius: 44,
+              borderTopLeftRadius: 44,
+              borderTopRightRadius: 0,
+          };
+       } else if(!bottom && !reverse){
+           return {
+               top: -34,
+               right:30,
+               borderBottomLeftRadius: 44,
+               borderBottomRightRadius: 0,
+               borderTopLeftRadius: 44,
+               borderTopRightRadius: 44,
+           };
+       } else if(!bottom && reverse){
+           return {
+               top: -34,
+               left:30,
+               borderBottomLeftRadius: 0,
+               borderBottomRightRadius: 44,
+               borderTopLeftRadius: 44,
+               borderTopRightRadius: 44,
+           };
+       }
+    }
+
+    doRender(beforeType, childType){
+        if(beforeType[0] === 0 && childType === 1){
+            return true;
+        }
+        if(beforeType.length > 1 && beforeType[1] === 0 && childType === 1){
+            return true;
+        }
+        return false;
+    }
+
+    renderNone(){
+        return (<View>
+                {this.props.children}
+            </View>);
     }
 
     render() {
+        if(Array.isArray(this.props.doRender) &&
+            ((this.props.doRender[0] === true || this.props.doRender[0] === false) ||
+            (this.props.doRender[1] === true || this.props.doRender[1] === false)) &&
+            !this.doRender(this.props.cardBeforeType, this.props.childCardType)){
+                return this.renderNone();
+        }
+
         return (
             <View>
                 {this.isTop(this.props.bottom)}
-                <View style={[styles.notificationBoubble, this.getBorderRadius(this.props.bottom), this.getWidth(this.props.widthMode), { backgroundColor: this.props.color }]}>
+                <View style={[styles.notificationBoubble, this.getBorderRadius(this.props.bottom, this.props.reverse), this.getWidth(this.props.widthMode), { backgroundColor: this.props.color }]}>
                     {this.isMultible(this.props.type, this.props.stats)}
 
                 </View>
@@ -66,7 +111,7 @@ export default class NotificationBoubble extends Component{
             let info = [];
             for (let i = 0; i < types.length; i++) {
                 info.push(
-                    <Text style={styles.notificationBoubbleText}>{stats[i]}</Text>);
+                    <Text key={i} style={styles.notificationBoubbleText}>{stats[i]}</Text>);
                 info.push(type[i]);
             }
 
