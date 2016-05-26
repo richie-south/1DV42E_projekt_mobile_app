5db13f48f4a46edbe9b90e287cf2b50146057cd9
rs222kn <rs222kn@student.lnu.se>
2016-05-26 11:51:09 +0200

    fixed react error and split render in two

--- IvyTown/components/Card.js
+++ IvyTown/components/Card.js
@@ -1,12 +1,7 @@
 'use strict';
 
-import React, {
-    Component,
-    Text,
-    Image,
-    View,
-    TouchableNativeFeedback
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, Text, Image, View, TouchableNativeFeedback} from 'react-native';
 
 import styles from '../styles/MyCardsStyle';
 import CardStats from './card/CardStats';
@@ -28,32 +23,36 @@ export default class Card extends Component{
               />
               <View style={styles.rightContainer}>
                 <Text style={styles.name}>{this.props.card.name}</Text>
-                <View>
-
-                    <View style={styles.statsWrap}>
-                        <Image
-                            source={require('../images/sword_icon.png')}
-                            style={[styles.statsImage, {tintColor: 'rgba(255,255,255, 0.54)' }]} />
-                        <CardStats type={'attack'} stats={this.props.card.stats.attack} />
-                    </View>
-
-                    <View style={styles.statsWrap}>
-                        <Image
-                            source={require('../images/bottle_icon.png')}
-                            style={[styles.statsImage, {tintColor: 'rgba(255,255,255, 0.54)' }]} />
-                        <CardStats type={'heal'} stats={this.props.card.stats.heal} />
-                    </View>
-
-                    <View style={styles.statsWrap}>
-                        <Image
-                            source={require('../images/shield_icon.png')}
-                            style={[styles.statsImage, {tintColor: 'rgba(255,255,255, 0.54)' }]} />
-                        <CardStats type={'block'} stats={this.props.card.stats.block} />
-                    </View>
-
-                </View>
+                {this.renderCardBottomPart()}
               </View>
             </View>
         );
+    }
+
+    renderCardBottomPart(){
+        return (<View>
+
+            <View style={styles.statsWrap}>
+                <Image
+                    source={require('../images/sword_icon.png')}
+                    style={[styles.statsImage, {tintColor: 'rgba(255,255,255, 0.54)' }]} />
+                <CardStats type={'attack'} stats={this.props.card.stats.attack} />
+            </View>
+
+            <View style={styles.statsWrap}>
+                <Image
+                    source={require('../images/bottle_icon.png')}
+                    style={[styles.statsImage, {tintColor: 'rgba(255,255,255, 0.54)' }]} />
+                <CardStats type={'heal'} stats={this.props.card.stats.heal} />
+            </View>
+
+            <View style={styles.statsWrap}>
+                <Image
+                    source={require('../images/shield_icon.png')}
+                    style={[styles.statsImage, {tintColor: 'rgba(255,255,255, 0.54)' }]} />
+                <CardStats type={'block'} stats={this.props.card.stats.block} />
+            </View>
+
+        </View>);
     }
 }
